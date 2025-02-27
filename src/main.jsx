141be786c682cb8dce4f75a129aae18143c9a476
Line Svensen <144799112+LineSvensen@users.noreply.github.com>
2025-02-27 09:56:51 +0100

    newnew

--- src/main.jsx
+++ src/main.jsx
@@ -15,12 +15,6 @@ const router = createBrowserRouter([
   },
 ]);
 
-function App() {
-  return <RouterProvider router={router} />;
-}
-
-export default App;
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
