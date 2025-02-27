fa03b736fd44e329c046b64eb1189d4ed22acf2d
Line Svensen <144799112+LineSvensen@users.noreply.github.com>
2025-02-27 09:43:34 +0100

    test

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1>I have successfully deployed with react !!:) </h1>
+    </>
+  )
+}
+
+export default App
